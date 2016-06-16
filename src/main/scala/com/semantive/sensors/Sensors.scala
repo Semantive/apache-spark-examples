2b7fb4b11b223c01b79256004b3340fc12c99a99
Anna Stępień <anna.stepien@semantive.com>
2016-06-16 10:40:07 +0200

    Small refactoring

--- src/main/scala/com/semantive/sensors/Sensors.scala
+++ src/main/scala/com/semantive/sensors/Sensors.scala
@@ -4,22 +4,20 @@ import org.apache.spark.SparkContext
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
 
 trait Sensors {
 
   def numClasses = 6
+
   def categoricalFeatures = Map[Int, Int]()
 
   def prepareData(sc: SparkContext) = {
-    val trainX = sc.textFile("/sensors-dataset/data/train/X_train.txt")
-      .map(l => l.split("\\s+").filterNot(_.isEmpty).map(_.toDouble)).zipWithIndex().map(_.swap)
-    val trainY = sc.textFile("/sensors-dataset/data/train/y_train.txt")
-      .map(l => l.split(" ").filterNot(_.isEmpty).map(_.toDouble)).zipWithIndex().map(_.swap)
+    val trainX = prepareRdd(sc.textFile("/sensors-dataset/data/train/X_train.txt"))
+    val trainY = prepareRdd(sc.textFile("/sensors-dataset/data/train/y_train.txt"))
 
-    val testX = sc.textFile("/sensors-dataset/data/test/X_test.txt")
-      .map(l => l.split("\\s+").filterNot(_.isEmpty).map(_.toDouble)).zipWithIndex().map(_.swap)
-    val testY = sc.textFile("/sensors-dataset/data/test/y_test.txt")
-      .map(l => l.split(" ").filterNot(_.isEmpty).map(_.toDouble)).zipWithIndex().map(_.swap)
+    val testX = prepareRdd(sc.textFile("/sensors-dataset/data/test/X_test.txt"))
+    val testY = prepareRdd(sc.textFile("/sensors-dataset/data/test/y_test.txt"))
 
     val train = trainX.join(trainY).map {
       case (i, (values, labels)) =>
@@ -32,6 +30,9 @@ trait Sensors {
     val Array(test, validation) = testAndValidation.randomSplit(Array(0.8, 0.2))
     (train, test, validation)
   }
+
+  private def prepareRdd(rdd: RDD[String]) =
+    rdd.map(l => l.split("\\s+").filterNot(_.isEmpty).map(_.toDouble)).zipWithIndex().map(_.swap)
 
   def evaluateModel(metrics: MulticlassMetrics) = {
     (0 until 6).map(
